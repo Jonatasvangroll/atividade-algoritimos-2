194924fa27802646740ab28cd3d40cd72d8d062d
Jonatasvangroll <jonatas.vangroll@gmail.com>
2022-03-09 20:37:31 -0300

    Update Atividade2.java

--- Atividade2.java
+++ Atividade2.java
@@ -19,4 +19,37 @@ public class Atividade2{
         return somatorio(i-1)+i;
     
     }
+    
+     public static String convBase2(int n){
+          
+          if(n<0){
+              return "Erro";
+            
+        }
+
+        if(n==0){
+            return "0";
+
+        }
+
+        if(n==1){
+            return "1";
+
+        }
+
+        int remainder=n%2;
+        int result=n/2;
+        String binary;
+
+        if(remainder==1){
+            binary="1";
+        }
+        else{
+            binary="0";
+        }
+
+        return convBase2(result) + binary;
+
+
+    }
 }
